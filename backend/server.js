596c18a82b80752f1dc0fbf8e8674a8c6e4bd65d
ksimran-935 <sainisimran9305@gmail.com>
2026-06-14 12:38:50 +0530

    Fix CORS for deployed frontend

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 connectDB();
 
 // Middleware
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:3000'];
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:3000' , 'https://hcms-project-frontend.onrender.com/'];
 if (process.env.FRONTEND_URL) {
   allowedOrigins.push(process.env.FRONTEND_URL);
   allowedOrigins.push(process.env.FRONTEND_URL.replace(/\/$/, ''));
